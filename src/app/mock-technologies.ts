22664765e13345dfb5f877d32abb6769bcf65613
qrames <ramque@orange.fr>
2018-11-24 12:57:03 +0100

    the technologie content my github url

--- src/app/mock-technologies.ts
+++ src/app/mock-technologies.ts
@@ -5,21 +5,25 @@ export const TECHNOLOGIES: Technologie[] = [
     'name': 'Angular',
     'class': "fab fa-angular",
     'progress': 30,
+    'url': "https://github.com/qrames/cvAngular",
   },
   {
     'name': 'Django',
     'class': "fab fa-python",
     'progress': 80,
+    'url': "",
   },
   {
     'name': 'Prestashop',
     'class': "fab fa-php",
     'progress': 20,
+    'url': "",
   },
   {
     'name': 'Django-CMS',
     'class': "fab fa-python",
     'progress': 60,
-  }
+    'url': "",
+  },
 
 ];
--- src/app/technologie.ts
+++ src/app/technologie.ts
@@ -2,4 +2,5 @@ export class Technologie {
   name: string;
   class: string;
   progress: number;
+  url: string;
 }
